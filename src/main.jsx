b85bd581f32fbca0d1c8d1d9d79b1748c7350aec
Aleeza Rubab <malikaleeza886@gmail.com>
2025-01-04 06:48:12 -0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { router } from './router'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import { Feature } from './routes/Feature'
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/features', element: <Feature /> },
 
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
